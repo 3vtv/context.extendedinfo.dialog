642d6d097abd3106724b061a86b69d4dc7355eb8
3vtv <90260969+3vtv@users.noreply.github.com>
2024-10-01 12:17:48 +0200

    v1.1.0 - plugin reactivation

--- addon.py
+++ addon.py
@@ -8,6 +8,9 @@ def main():
     elif xbmc.getCondVisibility('String.IsEqual(ListItem.DBType,movie)'):
         xbmc.executebuiltin('RunScript(script.extendedinfo,info=extendedinfo,name=\'"%s"\',year=%s)' % (xbmc.getInfoLabel('ListItem.Title'),xbmc.getInfoLabel('ListItem.Year')))
 
+    elif xbmc.getCondVisibility('String.IsEqual(ListItem.DBType,tvshow) + !String.IsEmpty(ListItem.IMDBNumber) + !String.StartsWith(ListItem.IMDBNumber,tt)'):
+        xbmc.executebuiltin('RunScript(script.extendedinfo,info=extendedtvinfo,id=%s)' % xbmc.getInfoLabel('ListItem.IMDBNumber'))
+
     elif xbmc.getCondVisibility('String.IsEqual(ListItem.DBType,tvshow)'):
         xbmc.executebuiltin('RunScript(script.extendedinfo,info=extendedtvinfo,name=\'"%s"\',name=\'"%s"\',year=%s)' % (xbmc.getInfoLabel('ListItem.TVShowTitle'),xbmc.getInfoLabel('ListItem.OriginalTitle'),xbmc.getInfoLabel('ListItem.Year')))
 
